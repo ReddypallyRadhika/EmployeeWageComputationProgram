4a5fc5c5e897a8ad845c400fd311370011ec94e3
Radhika <reddypallyradhika@gmail.com>
2022-09-07 09:42:03 +0530

    Refactor Employee Wage Uc1

--- src/com/bridgelabz/EmployeeWageUc1.java
+++ src/com/bridgelabz/EmployeeWageUc1.java
@@ -11,14 +11,26 @@ import java.util.Random;
  */
 public class EmployeeWageUc1{
 
-	/**
+	/**UC1
+	 * Check Employee is
+     *Present or Absent
+-    *Use ((RANDOM)) for Attendance Check*/
+	/*
+	 * UC2
+	 * Calculate Daily Employee Wage
+     *Assume Wage per hour is 20
+     *Assume full day hr is 8
 	 * @param args
 	 */
+	/*UC3
+	Employee Wage*/
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int Wageperhr =20;
+		int Fulldayhr =8;
+		int DailyEmpWage =0;
 Random random =new Random();
-		
-		int EmpPresent=random.nextInt(0,2);
+		int EmpPresent=random.nextInt(0,3);
 		if(EmpPresent==1)
 		{
 		System.out.println("Employee is present");
@@ -27,6 +39,8 @@ Random random =new Random();
 		{
 		System.out.println("Employee is absent");
 	}
+		DailyEmpWage = Wageperhr*Fulldayhr;
+		System.out.println("Daily Employee Wage:"+DailyEmpWage);
 	}
 
 }
