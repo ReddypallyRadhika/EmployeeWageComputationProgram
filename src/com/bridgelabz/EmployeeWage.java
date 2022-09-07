0a1085b5e2c2f1c43f4fad103037e601a00b3c81
Radhika <reddypallyradhika@gmail.com>
2022-09-07 10:55:57 +0530

    Added:Part Tme Employee Wage:UC3

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -26,18 +26,30 @@ public class EmployeeWage {
      *Assume full day hr is 8
 	 */
 	
+	/*UC3
+	*Add Part time Employee & Wage
+    *- Assume Part time Hour is 4
+    **/
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int Wageperhr =20;
 		int Fulldayhr =8;
 		int DailyEmpWage =0;
+		int PartTime =4;
         Random random =new Random();
 		int EmpPresent=random.nextInt(0,3);
-		DailyEmpWage = Wageperhr*Fulldayhr;
 		
 		if(EmpPresent==1)
 		{
-		System.out.println("Employee is present");
+		System.out.println("Employee full day present");
+		DailyEmpWage = Wageperhr*Fulldayhr;
+		System.out.println("Daily Employee Wage:"+DailyEmpWage);
+		}
+		else if(EmpPresent==2)
+		{
+		System.out.println("Employee half day present");
+		DailyEmpWage = Wageperhr*PartTime;
 		System.out.println("Daily Employee Wage:"+DailyEmpWage);
 		}
 		else
