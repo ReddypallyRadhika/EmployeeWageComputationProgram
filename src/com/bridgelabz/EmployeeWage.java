beb1ce6709ff03b9555df3bafad6c98d0e71169b
Radhika <reddypallyradhika@gmail.com>
2022-09-07 10:39:02 +0530

    Added:Calculate Daily Emloyee Wage:UC2

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -19,13 +19,26 @@ public class EmployeeWage {
      *Present or Absent
 -    *Use ((RANDOM)) for Attendance Check*/
 	
+	/*
+	 * UC2
+	 * Calculate Daily Employee Wage
+     *Assume Wage per hour is 20
+     *Assume full day hr is 8
+	 */
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Random random =new Random();
+		int Wageperhr =20;
+		int Fulldayhr =8;
+		int DailyEmpWage =0;
+        Random random =new Random();
 		int EmpPresent=random.nextInt(0,3);
+		DailyEmpWage = Wageperhr*Fulldayhr;
+		
 		if(EmpPresent==1)
 		{
 		System.out.println("Employee is present");
+		System.out.println("Daily Employee Wage:"+DailyEmpWage);
 		}
 		else
 		{
